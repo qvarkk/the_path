8e1d356fd4744c193c91a6bfdf1aae76715bf6d4
qvarkk <xumdes@gmail.com>
2022-07-18 13:28:00 +0500

    implemented bacis button functions

--- ConsoleRockPaperScissors/script.js
+++ ConsoleRockPaperScissors/script.js
@@ -1,8 +1,9 @@
-alert('Open console by pressing F12 to play the game (you may need to refresh the page). You can quit by pressing Esc');
-
-console.log('SCORE IS IN YOU:COMP TYPE');
-
 const results = ['rock', 'paper', 'scissors']; // Elements for "computer" to loop through 🤯
+const paraScore = document.querySelector('.show_score');
+const paraResult = document.querySelector('.show_result');
+const paraGameResult = document.querySelector('.game_result');
+
+paraResult.innerHTML = 'Press a button to choose an element and start a game!'
 
 // "Computer" makes play 😵‍💫
 function computerPlay() {
@@ -12,80 +13,97 @@ function computerPlay() {
 
 // Goes through all possible game results 🤓
 function playRound(playerSelection, computerSelection) {
-    // In case of wrong input 🙄
-    if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
-        alert('Incorrect input! Try again!');
-        return 'input error';
-    }
-    
     if (computerSelection === 'rock'){
         if (playerSelection === 'rock') {
-            console.log(`You've chosen ${playerSelection}. Computer choise was ${computerSelection}. It's a tie in this round!`);
+            paraResult.innerHTML = `You've chosen <span class="selection">${playerSelection}</span>. Computer choise was <span class="selection">${computerSelection}</span>. It's a tie in this round!`;
             return 'tie';
         } else if (playerSelection === 'paper') {
-            console.log(`You've chosen ${playerSelection}. Computer choise was ${computerSelection}. You've won this round!`);
+            paraResult.innerHTML = `You've chosen <span class="selection">${playerSelection}</span>. Computer choise was <span class="selection">${computerSelection}</span>. You've won this round!`;
             return 'win';
         } else if (playerSelection === 'scissors') {
-            console.log(`You've chosen ${playerSelection}. Computer choise was ${computerSelection}. You've lost this round!`);
-            return 'loss';
+            paraResult.innerHTML = `You've chosen <span class="selection">${playerSelection}</span>. Computer choise was <span class="selection">${computerSelection}</span>. You've lost this round!`;
+            return 'lose';
         }
     } else if (computerSelection === 'paper') {
         if (playerSelection === 'rock') {
-            console.log(`You've chosen ${playerSelection}. Computer choise was ${computerSelection}. You've lost this round!`);
-            return 'loss';
+            paraResult.innerHTML = `You've chosen <span class="selection">${playerSelection}</span>. Computer choise was <span class="selection">${computerSelection}</span>. You've lost this round!`;
+            return 'lose';
         } else if (playerSelection === 'paper') {
-            console.log(`You've chosen ${playerSelection}. Computer choise was ${computerSelection}. It's a tie in this round!`);
+            paraResult.innerHTML = `You've chosen <span class="selection">${playerSelection}</span>. Computer choise was <span class="selection">${computerSelection}</span>. It's a tie in this round!`;
             return 'tie';
         } else if (playerSelection === 'scissors') {
-            console.log(`You've chosen ${playerSelection}. Computer choise was ${computerSelection}. You've won this round!`);
+            paraResult.innerHTML = `You've chosen <span class="selection">${playerSelection}</span>. Computer choise was <span class="selection">${computerSelection}</span>. You've won this round!`;
             return 'win';
         }
     } else if (computerSelection === 'scissors') {
         if (playerSelection === 'rock') {
-            console.log(`You've chosen ${playerSelection}. Computer choise was ${computerSelection}. You've won this round!`);
+            paraResult.innerHTML = `You've chosen <span class="selection">${playerSelection}</span>. Computer choise was <span class="selection">${computerSelection}</span>. You've won this round!`;
             return 'win';
         } else if (playerSelection === 'paper') {
-            console.log(`You've chosen ${playerSelection}. Computer choise was ${computerSelection}. You've lost this round!`);
-            return 'loss';
+            paraResult.innerHTML = `You've chosen <span class="selection">${playerSelection}</span>. Computer choise was <span class="selection">${computerSelection}</span>. You've lost this round!`;
+            return 'lose';
         } else if (playerSelection === 'scissors') {
-            console.log(`You've chosen ${playerSelection}. Computer choise was ${computerSelection}. It's a tie in this round!`);
+            paraResult.innerHTML = `You've chosen <span class="selection">${playerSelection}</span>. Computer choise was <span class="selection">${computerSelection}</span>. It's a tie in this round!`;
             return 'tie';
         }
     }
 }
 
+// 
+// // Loops for 1000 times (basically plays 1000 games) and breaks if one side wins 🥳
+// for (let i = 0; i < 1000; i++) {
+//     let playerSelection;
+//     // If user closes the prompt instead of throwing an error it will break out of game loop 😉
+//     try {
+//         playerSelection = prompt('Enter rock, paper or scissors').toString().toLowerCase();
+//     }
+//     catch (TypeError) {
+//         console.log('Refresh the page if you want to play again');
+//         break;
+//     }
+
+//     let result = playRound(playerSelection, computerPlay());
+//     if (result === 'win') {
+//         playerScore++;
+//         console.log(`Now the score is ${playerScore}:${compScore}`);
+//     } else if (result === 'loss') {
+//         compScore++;
+//         console.log(`Now the score is ${playerScore}:${compScore}`)
+//     } else if (result === 'input error') {
+//         console.log('Input error: try again!')
+//     } else {
+//         console.log(`Score remains the same ${playerScore}:${compScore}`)
+//     }
+
+//     if (playerScore === 5) {
+//         console.log("You've won! Refresh the page to play again")
+//         break;
+//     } else if (compScore === 5) {
+//         console.log("You've lost! Refresh the page to play again")
+//         break;
+//     }
+// }
+
+const btn_rock = document.querySelector('.rock');
+const btn_paper = document.querySelector('.paper');
+const btn_scissors = document.querySelector('.scissors');
 let playerScore = 0;
 let compScore = 0;
-// Loops for 1000 times (basically plays 1000 games) and breaks if one side wins 🥳
-for (let i = 0; i < 1000; i++) {
-    let playerSelection;
-    // If user closes the prompt instead of throwing an error it will break out of game loop 😉
-    try {
-        playerSelection = prompt('Enter rock, paper or scissors').toString().toLowerCase();
-    }
-    catch (TypeError) {
-        console.log('Refresh the page if you want to play again');
-        break;
-    }
 
-    let result = playRound(playerSelection, computerPlay());
-    if (result === 'win') {
-        playerScore++;
-        console.log(`Now the score is ${playerScore}:${compScore}`);
-    } else if (result === 'loss') {
-        compScore++;
-        console.log(`Now the score is ${playerScore}:${compScore}`)
-    } else if (result === 'input error') {
-        console.log('Input error: try again!')
-    } else {
-        console.log(`Score remains the same ${playerScore}:${compScore}`)
+const selectRock = () => {
+    if (playRound('rock', computerPlay()) === 'win') {
+        playerScore += 1;
+    } else if (playRound('rock', computerPlay()) === 'lose') {
+        compScore += 1;
     }
+};
+const selectPaper = () => {
+    playRound('paper', computerPlay());
+};
+const selectScissors = () => {
+    playRound('scissors', computerPlay());
+};
 
-    if (playerScore === 5) {
-        console.log("You've won! Refresh the page to play again")
-        break;
-    } else if (compScore === 5) {
-        console.log("You've lost! Refresh the page to play again")
-        break;
-    }
-}
+btn_rock.addEventListener('click', selectRock);
+btn_paper.addEventListener('click', selectPaper);
+btn_scissors.addEventListener('click', selectScissors);
